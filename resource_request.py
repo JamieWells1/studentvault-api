368e73827251e1e235133136bc00da90b31c4dfb
Jamie Wells <jamiewells528@gmail.com>
2025-02-16 11:36:22 +0000

    readded gunicorn dependency

--- resource_request.py
+++ resource_request.py
@@ -19,9 +19,9 @@ video_ids = [
 selected_id = video_ids[random.randint(0, len(video_ids) - 1)]
 
 video_id = selected_id
-generation_method = "video"
+generation_method = "text"
 text_prompt = "Robert J. Oppenheimer"
-resource_type = "lesson"
+resource_type = "quiz"
 
 headers = {"Content-Type": "application/json"}
 body = {
